4c3585e7b019a0917d57543d5d14a9d3e9411a13
Yusuf <yusssuf0786@gmail.com>
2024-10-12 16:44:19 +0530

    base css added

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(err => {
-          console.log('Service Worker registration failed:', err);
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch(err => {
+            console.log('Service Worker registration failed:', err);
         });
     });
-  }
-
-  
+}
 
 let deferredPrompt;
 const installBtn = document.getElementById('installBtn');
